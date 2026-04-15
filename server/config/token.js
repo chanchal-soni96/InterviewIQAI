6ab6791df9816868a8063d8183bd46cce19f38a8
chanchal soni <chanchalmsk1@gmail.com>
2026-04-15 18:18:44 +0530

    Redux Setup

--- server/config/token.js
+++ server/config/token.js
@@ -2,7 +2,7 @@
 import jwt from "jsonwebtoken"
 const genToken = async(userId) => {
     try {
-      const token = jwt.sign({userId}, process.env.JWT_SECRET, {expireIn:"7d"})
+      const token = jwt.sign({userId}, process.env.JWT_SECRET, {expiresIn:"7d"})
       return token
   
     } catch (error) {
