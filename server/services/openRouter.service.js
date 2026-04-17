b46b05999cc1b13927454c3eb48b75ff0bc052c8
chanchal soni <chanchalmsk1@gmail.com>
2026-04-17 16:38:32 +0530

    Open router setup

---
+++ server/services/openRouter.service.js
@@ -0,0 +1,30 @@
+export const askAi = async (message) => {
+  try {
+    if(!message || !Array.isArray(message) || message.length === 0) {
+      throw new Error("Messages array is empty.");
+    }
+    const response = await axios.post(
+      "https://openrouter.ai/api/v1/chat/completions",
+      {
+        model: "openai/gpt-40-mini",
+        messages: messages
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${process.env.OPENROUTER_API_KEY}`,
+          
+          "Content-Type": "application/json",
+        },});
+        const content = response?.data?.choices?.[0]?.message?.content;
+
+        if(!content || !content.trim()){
+          throw new Error("AI returned empty response.");
+        }
+        return Content
+  } catch (error) {
+    console.log("OpenRouter Error:", error.response?.data || error.message);
+    throw new Error("OpenRouter API Error");
+    
+
+  }
+};
