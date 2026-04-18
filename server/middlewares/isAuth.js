da54a4b521f4a8b56ecbde41e33274968c3a9baa
chanchal soni <chanchalmsk1@gmail.com>
2026-04-19 00:05:09 +0530

    Creating Step 1 Setup Component

--- server/middlewares/isAuth.js
+++ server/middlewares/isAuth.js
@@ -2,20 +2,24 @@ import jwt from "jsonwebtoken";
 
 const isAuth = async(req, res, next) => {
     try {
-        let {token} = req.cookies
+        let {token} = req.cookies;
 
         if(!token){
-            return res.status(400).json({message:"user does not have a token"})
+            return res.status(401).json({message:"user does not have a token"})
         }
         const verifyToken = jwt.verify(token, process.env.JWT_SECRET)
     if(!verifyToken){
-        return res.status(400).json({message:"user does not have a valid token"})
+        return res.status(401).json({message:"user does not have a valid token"})
     }
     req.userId = verifyToken.userId
     next()
     } catch (error) {
-       return res.status(500).json({message: `isAuth error ${error}`})
-
-    }
+     return res.status(500).json({message: `isAuth error ${error}`})
+    
+     //  console.log("Auth Error:", error.message);
+     // return res.status(401).json({ 
+    //   message: "Token expired or invalid" 
+    // })
+}
 }
 export default isAuth
--- server/services/openRouter.service.js
+++ server/services/openRouter.service.js
@@ -1,12 +1,13 @@
-export const askAi = async (message) => {
+import axios from "axios";
+export const askAi = async (messages) => {
   try {
-    if(!message || !Array.isArray(message) || message.length === 0) {
+    if(!messages || !Array.isArray(messages) || messages.length === 0) {
       throw new Error("Messages array is empty.");
     }
     const response = await axios.post(
       "https://openrouter.ai/api/v1/chat/completions",
       {
-        model: "openai/gpt-40-mini",
+        model: "openai/gpt-4o-mini",
         messages: messages
       },
       {
@@ -20,7 +21,7 @@ export const askAi = async (message) => {
         if(!content || !content.trim()){
           throw new Error("AI returned empty response.");
         }
-        return Content
+        return content
   } catch (error) {
     console.log("OpenRouter Error:", error.response?.data || error.message);
     throw new Error("OpenRouter API Error");
