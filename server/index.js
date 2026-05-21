a02f606bf95419b6cbc1a9a3fd2c9a8b05062abd
chanchal-soni96 <chanchalmsk1@gmail.com>
2026-05-22 03:35:17 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ import paymentRouter from "./routes/payment.route.js"
 
 const app = express()
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://interviewiqai-1client.onrender.com",
     credentials:true
 }))
 
